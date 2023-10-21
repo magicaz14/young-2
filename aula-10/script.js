48006d91cab0ef5a1ef63346601f8fd43fd05faf
magicaz14 <82606233+magicaz14@users.noreply.github.com>
2023-10-21 09:41:25 -0300

    formulario

--- aula-10/script.js
+++ aula-10/script.js
@@ -8,15 +8,36 @@ const media_saida = document.getElementById('media')
 const resultado_saida = document.getElementById('resultado')
 const formulario = document.getElementById('formulario')
 
-formulario.addEventListener('submit', (e) =>})
+formulario.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    console.log(media())
+})
 
 function media(){
-   let n1 = parseInt(num1.value)
-   let n2 = parseInt(num2.value)
-   let n3 = parseInt(num3.value)
 
-    return n1 + n2 + n3 
+    let nome = nome_input.value
+    let n1 = parseInt(num1.value)
+    let n2 = parseInt(num2.value)
+    let n3 = parseInt(num3.value)
+
+    // localStorage.setItem('nome', nome)
+    // localStorage.setItem('n1', n1)
+    // localStorage.setItem('n2', n2)
+    // localStorage.setItem('n3', n3)
+    
+    let resultado = (n1 + n2 + n3) / 3
+    nome_saida.innerHTML = nome
+    media_saida.innerHTML = resultado
+
+    if (resultado >= 7){
+        resultado_saida.innerHTML = 'aprovado'
+    }
+    else if (resultado_saida <= 4)
+        resultado_saida.innerHTML = 'reprovado'
+    else{
+        resultado_saida.innerHTML = 'recuperação'
+    }
+
 }
 
-let x = media()
-console.log(x)

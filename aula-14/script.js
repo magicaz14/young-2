ae1b963e88c34dd8eb1e4427f32261bf5a385d01
Arthur Castro Silva <82606233+magicaz14@users.noreply.github.com>
2023-12-14 11:02:02 -0300

    pokedex-final

--- aula-14/script.js
+++ aula-14/script.js
@@ -1,10 +1,10 @@
-const NAME_POKE = document.querySelector('.num_poke')
+const NAME_POKE = document.querySelector('.name_poke')
 const NUMBER_POKE = document.querySelector('.num_poke')
 const IMG_POKE = document.querySelector('.poke-img')
 const FORM = document.querySelector('.forms')
 const INPUT = document.querySelector('.input_search')
 const PREV_BTN = document.querySelector('.pre_btn')
-const NEXT_BNT = document.querySelector('.next_btn')
+const NEXT_BNT = document.querySelector('.next-btn')
 
 let id_pokemon = 1
 
@@ -20,6 +20,34 @@ const mostrar_pokemon = async (pokemon) =>{
     const json_pokemon = await buscar_pokemon(pokemon)
     if (json_pokemon){
         IMG_POKE.src = json_pokemon['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+        IMG_POKE.style.display = 'block'
+        NAME_POKE.innerHTML = json_pokemon.name
+        NUMBER_POKE.innerHTML = json_pokemon.id
+        id_pokemon = json_pokemon.id
+        INPUT.value = ''
+    }
+    else{
+        IMG_POKE.style.display = 'none'
+        NAME_POKE.innerHTML = 'not Founded'
+        NUMBER_POKE.innerHTML =''
     }
 }
-mostrar_pokemon('arceus')
+
+FORM.addEventListener('submit', (evento) =>{
+    evento.preventDefault()
+    let pokemon = INPUT.value.toLowerCase()
+    mostrar_pokemon(pokemon)
+})
+
+PREV_BTN.addEventListener('click', ()=>{
+    if (id_pokemon > 1){
+        id_pokemon -= 1
+        mostrar_pokemon(id_pokemon)
+    }
+})
+
+NEXT_BNT.addEventListener('click', () =>{
+    id_pokemon +=1
+    mostrar_pokemon(id_pokemon)
+})
+mostrar_pokemon('id_pokemon')
